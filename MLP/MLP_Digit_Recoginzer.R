546b7c2dee60694176c2447d01793b69a00ec7ab
vle <ql.viet@gmail.com>
2019-02-01 20:08:36 -0500

    rewrote NN model for 98% validation accuracy and got a 95% submission score

--- MLP/MLP_Digit_Recoginzer.R
+++ MLP/MLP_Digit_Recoginzer.R
@@ -2,7 +2,7 @@ library(neuralnet)
 library(nnet)
 library(tictoc)
 
-data_file.path <- "../data"
+data_file.path <- "../input"
 data_file.train <- paste(data_file.path, "train.csv", sep="/")
 data_file.test <- paste(data_file.path, "test.csv", sep="/")
 
@@ -22,60 +22,36 @@ names(training.scaled) <- c(onehot_labels.names, names(training.scaled)[11:794])
 
 pixels.sum <- colSums(training.scaled[,11:794])
 pixels.norm <- pixels.sum/max(pixels.sum)
-
 pixels.select <- names(pixels.norm[pixels.norm > 0.05])
+
+
 toc()
 
 f <- as.formula(paste(paste(onehot_labels.names, collapse = '+'), paste(pixels.select, collapse = '+'), sep = '~'))
+accuracy <- NULL
+weights <- NULL
+K = 10
+tic("Total Training Time")
+for (k in 1:K)
+{
+    index <- sample(1:42000, 0.9 * 42000)
+    tr <- training.scaled[index, ]
+    vl <- training.scaled[-index, ]
+    tic(paste("Epoch", k))
+    print(paste("Starting Epoch", k))
+    nn.model <- neuralnet(f, data = tr, hidden = c(128, 64), startweights = weights, linear.output = FALSE, lifesign = 'full')
+    weights <- nn.model$weights
+    toc()
+    nn.predict <- compute(nn.model, vl[, pixels.select])
 
-# training.data <- training.scaled[1:4200,]
-# training.validation <- training.scaled[4201:8400,]
-# tic("Total")
-# tic("epoch 1")
-# print("epoch 1")
-# nn.model <- neuralnet(f, data = training.data, hidden = c(128, 64), linear.output = FALSE)
-# nn.predict <- compute(nn.model, training.validation[, pixels.select])
-# accuracy <- NULL
-# accuracy[1] <- sum((max.col(nn.predict$net.result) - 1) == digits.train$label[4201:8400])/4200
-# print(accuracy)
-# toc(func.toc = function(tic, toc, msg) outmsg <- paste(msg, "done: ", round(round(toc - tic, 3)/60, 2), "minutes"))
-# 
-# K = 10
-# for (k in 2:(K-1)) {
-#   tic(paste("epoch", k))
-#   print(paste("epoch", k, Sys.time()))
-#   training.data <- training.scaled[(1 + (k-1)*4200):(k*4200), ]
-#   training.validation <- training.scaled[(1 + k*4200):((k+1)*4200), ]
-#   nn.model <- neuralnet(f, data=training.data, hidden = c(128, 64), linear.output = FALSE, startweights = nn.model$weights)
-#   nn.predict <- compute(nn.model, training.validation[, pixels.select])
-#   accuracy[k] <- sum((max.col(nn.predict$net.result) - 1) == digits.train$label[(1 + k*4200):((k+1)*4200)])/4200
-#   print(accuracy)
-#   toc(func.toc = function(tic, toc, msg) outmsg <- paste(msg, "done: ", round(round(toc - tic, 3)/60, 2), "minutes"))
-# }
-# 
-# k=10
-# tic("epoch 10")
-# print(paste("epoch 10", Sys.time()))
-# training.data <- training.scaled[(1 + (k-1)*4200):(k*4200), ]
-# training.validation <- training.scaled[1:4200, ]
-# nn.model <- neuralnet(f, data=training.data, hidden = c(128, 64), linear.output = FALSE, startweights = nn.model$weights)
-# nn.predict <- compute(nn.model, training.validation[, pixels.select])
-# accuracy[k] <- sum((max.col(nn.predict$net.result) - 1) == digits.train$label[1:4200])/4200
-# print(accuracy)
-# toc(func.toc = function(tic, toc, msg) outmsg <- paste(msg, "done: ", round(round(toc - tic, 3)/60, 2), "minutes"))
-# toc(func.toc = function(tic, toc, msg) outmsg <- paste(msg, "done: ", round(round(toc - tic, 3)/60, 2), "minutes"))
-
-index <- sample(1:42000, 0.9 * 42000)
-tr <- training.scaled[index, ]
-vl <- training.scaled[-index, ]
-tic("training model")
-nn.model <- neuralnet(f, data = tr, hidden = c(128, 64), linear.output = FALSE, lifesign = 'full')
+    accuracy[k] <- sum(max.col(nn.predict$net.result) - 1 == digits.train$label[-index])/4200
+    print(paste("Mean Accuracy:", mean(accuracy)))
+    print(paste("Accuracy Change:", accuracy[k] - accuracy[k-1]))
+}
 toc()
-nn.predict <- compute(nn.model, vl[, pixels.select])
 
 te <- digits.test/255
-
 nn.test <- compute(nn.model, te[, pixels.select])
 
-sub <- data.frame(ImageId = 1:28000, Label = max.col(nn.test$net.result))
+sub <- data.frame(ImageId = 1:28000, Label = max.col(nn.test$net.result) - 1)
 write.csv(sub, file = "submission.csv", row.names = FALSE, quote = FALSE)
